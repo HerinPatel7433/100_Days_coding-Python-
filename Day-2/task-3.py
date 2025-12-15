5642f2cbe2a0ae7067226c4152118f4072dcd8cb
Herin Patel <herinpatel6747@gmail.com>
2025-12-15 09:44:19 +0530

    Start Day-2 Python Code

---
+++ Day-2/task-3.py
@@ -0,0 +1,10 @@
+#mathematical operations
+
+print(123 + 456)  # Addition, will print 579
+print(123 - 456)  # Subtraction, will print -333
+print(123 * 456)  # Multiplication, will print 56088
+print(123 / 456)  # Division, will print approximately 0.26973684210526316
+print(123 // 456) # Floor Division, will print 0
+print(123 % 456)  # Modulus, will print 123
+print(2 ** 3)     # Exponentiation, will print 8
+print(9 ** (1/2))  # Square root, will print 3.0
