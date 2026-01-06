c89cad87cc468a808122459b189ac72fb37fc288
Herin Patel <herinpatel6747@gmail.com>
2026-01-06 20:30:37 +0530

    Day-28 Python Code

---
+++ Day-28/main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+PINK = "#e2979c"
+RED = "#e7305d"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+
+window = tk.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50)
+
+canvas = tk.Canvas(width=200, height=224)
+tomato_img = tk.PhotoImage(file="Day-28\\tomato.png")
+canvas.create_image(102, 112, image=tomato_img)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+window.mainloop()
+
