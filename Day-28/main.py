e8e3f39e5146d6c712b856b9341fa5a1e2a7bec9
Herin Patel <herinpatel6747@gmail.com>
2026-01-06 21:00:59 +0530

    Pomodoro GUI UI Added

--- Day-28/main.py
+++ Day-28/main.py
@@ -12,13 +12,25 @@ LONG_BREAK_MIN = 20
 
 window = tk.Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = tk.Canvas(width=200, height=224)
+title_label = tk.Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50, "bold"), bg=YELLOW, highlightthickness=0)
+title_label.grid(column=1, row=0)
+
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="Day-28\\tomato.png")
-canvas.create_image(102, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = tk.Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = tk.Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = tk.Label(text="✓", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
 
