8f49f84346e6e9e96050784252c1e8172c4716e9
Herin Patel <herinpatel6747@gmail.com>
2026-01-07 20:04:09 +0530

    Day-29 Python Code

---
+++ Day-29/main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = tk.Canvas(width=200, height=200, highlightthickness=0)
+lock_img = tk.PhotoImage(file="Day-29\\logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=1)
+
+window.mainloop()
+
