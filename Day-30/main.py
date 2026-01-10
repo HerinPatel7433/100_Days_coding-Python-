c1a744f4fdb7cdd93364cb98990f81c32216b971
Herin Patel <herinpatel6747@gmail.com>
2026-01-10 18:35:56 +0530

    Learning Error and Exceptions handlaing

--- Day-30/main.py
+++ Day-30/main.py
@@ -1,1 +1,16 @@
 """Error and Exceptions"""
+
+try:
+    file = open("Day-30//a_file.txt")
+    a_dictionary = {"key": "Value"}
+    print(a_dictionary["HERIN"])
+except FileNotFoundError:
+    file = open("Day-30//a_file.txt", "w")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was Closed.")
