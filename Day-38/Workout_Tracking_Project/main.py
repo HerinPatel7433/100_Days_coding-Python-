4b3c2ed43f05d33ad341122382984196761948ab
Herin Patel <herinpatel6747@gmail.com>
2026-03-01 22:03:45 +0530

    Add workout tracking functionality with API integration and environment variable support

---
+++ Day-38/Workout_Tracking_Project/main.py
@@ -0,0 +1,62 @@
+import requests
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+APP_ID = os.environ.get("NT_APP_ID")
+API_KEY = os.environ.get("NT_API_KEY")
+
+GENDER = "male"
+WEIGHT_KG = 69.2
+HEIGHT_CM = 173.0
+AGE = 19
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = os.environ.get("SHEET_ENDPOINT", "https://api.sheety.co/efa670550dface4cb9d02139d08c26fe/workoutTracking/workouts")
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+
+    bearer_headers = {
+        "Authorization": f"Bearer {os.environ.get('TOKEN',)}"
+    }
+
+    sheet_response = requests.post(
+        sheet_endpoint, 
+        json=sheet_inputs, 
+        headers=bearer_headers
+    )
+
+    print(sheet_response.text)
