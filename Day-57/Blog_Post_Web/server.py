bbc453f5a849cc00d07be1b2089aa0154e468688
Herin Patel <herinpatel6747@gmail.com>
2026-08-07 20:47:53 +0530

    Add Post class and templates for blog application

--- Day-57/Blog_Post_Web/server.py
+++ Day-57/Blog_Post_Web/server.py
@@ -1,35 +1,27 @@
 from flask import Flask, render_template
+from post import Post
 import requests
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
-BLOG_API_URL = "https://api.npoint.io/c790b4d5cab58020d391"
-
-def get_all_posts():
-    response = requests.get(BLOG_API_URL)
-    return response.json()
-
 @app.route('/')
-def home():
-    posts = get_all_posts()
-    return render_template('index.html', posts=posts)
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
 
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    posts = get_all_posts()
+
+@app.route("/post/<int:index>")
+def show_post(index):
     requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == post_id:
+    for blog_post in post_objects:
+        if blog_post.id == index:
             requested_post = blog_post
-    return render_template('post.html', post=requested_post)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
