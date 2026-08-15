2ff3bd7a61735f4faa3fb2d4920dac7952a7ed5d
Herin Patel <herinpatel6747@gmail.com>
2026-08-15 10:45:15 +0530

    Refactor templates to extend base layout and improve form validation

--- Day-61/main.py
+++ Day-61/main.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import Length, Email, DataRequired
+from flask_bootstrap import Bootstrap5
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email =  email = StringField('Email', [
+            Length(min=6, message=('Little short for an email address?')),
+            Email(message=('That\'s not a valid email address.'))
+    ])
+    password = PasswordField('Password', [Length(min=6, max=120), DataRequired()])
     submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
+
+bootstrap = Bootstrap5(app)
 
 
 @app.route("/")
@@ -18,9 +24,14 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=login_form)
 
 
